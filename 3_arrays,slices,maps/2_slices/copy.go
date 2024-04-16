1d54d10759edecbf78eac631b1ee9bb8b2433e70
Aaron <aaronjimv@gmail.com>
2024-04-16 14:11:05 -0700

    actualizar 2_slices/copy

--- 3_arrays,slices,maps/2_slices/copy.go
+++ 3_arrays,slices,maps/2_slices/copy.go
@@ -1,20 +1,20 @@
-package main 
+package main
 
 import "fmt"
 
 func main() {
-	
+
 	// copy copia el minimo de elementos de ambos arreglos
 	// copy(destino,fuente)
 
-	slice := [] int {1,2,3,4,5}
-	copia := make([]int,len(slice),cap(slice)*2)
+	slice := []int{1, 2, 3, 4, 5}
+	copia := make([]int, len(slice), cap(slice)*2)
 
 	fmt.Println(slice)
 	fmt.Println(copia)
 	fmt.Println("copiando...")
 
-	copy(copia,slice)
+	copy(copia, slice)
 
 	fmt.Println(slice)
 	fmt.Println(copia)
