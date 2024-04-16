db8d55e60a243c6e9078732e247c68c28898576d
Aaron <aaronjimv@gmail.com>
2024-04-16 12:39:47 -0700

    3/ actualizar arrglos

--- 3_arrays,slices,maps/1_arreglos/arreglos.go
+++ 3_arrays,slices,maps/1_arreglos/arreglos.go
@@ -1,31 +1,31 @@
-package main 
+package main
 
 import "fmt"
 
 func main() {
-	
+
 	//los arreglos en go son estaticos, se tiene que declarar el tamaño y tiene que ser el mismo tipo de dato
-	var arreglo [10] int
+	var arreglo [10]int
 
 	fmt.Println(arreglo)
 
-	// al hacerlo de esta forma hay que colocar {} adentro se puede poner el valor
-	arreglo2 := [3] int {}
+	// arrglo literal: al hacerlo de esta forma hay que colocar {} adentro se puede poner el valor
+	arreglo2 := [3]int{}
 
 	fmt.Println(arreglo2)
 
 	//para la longitub de usa len()
 	fmt.Println(len(arreglo))
 
-	for i:=0;i<len(arreglo);i++{
-		arreglo[i]=i
+	for i := 0; i < len(arreglo); i++ {
+		arreglo[i] = i
 	}
-	for i:=0;i<len(arreglo);i++{
+	for i := 0; i < len(arreglo); i++ {
 		fmt.Println(arreglo[i])
 	}
 
 	//las matrices son: var nombre[cantidad de arreglos][numero de valores]
-	var matriz[3][2] int
+	var matriz [3][2]int
 	fmt.Println(matriz)
 
 }
---
+++ 3_arrays,slices,maps/1_arreglos/arreglos_literal.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+// son un poco mas flexible ya que no hay que declarar el tamaño con anterioridad
+// se usa:nombre_arrglo := [...] tipo {valores}
+
+func main() {
+	// var flags [3]string
+	// flags[0] = "🎏"
+	// flags[1] = "🏴"
+	// flags[2] = "🏳️"
+
+	flags := [...]string{"🎏", "🏴", "🏳️", "🏴‍☠️", "🏴"}
+
+	// el tamaño despues de declarar, no se puede cambiear
+	// flags[6] = "🏁"
+
+	fmt.Println(flags)
+}
