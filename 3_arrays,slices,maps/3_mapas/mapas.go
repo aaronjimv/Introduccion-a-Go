fe6427fc67487d8dbebf4823a63defafc689841f
Aaron <aaronjimv@gmail.com>
2024-04-16 14:08:29 -0700

    actulizar mapas/

--- 3_arrays,slices,maps/3_mapas/mapas.go
+++ 3_arrays,slices,maps/3_mapas/mapas.go
@@ -1,4 +1,4 @@
-package main 
+package main
 
 import "fmt"
 
@@ -6,7 +6,6 @@ import "fmt"
 	los mapas son como los diccionarios de python se declaran:
 		nombre := make(map[tipo_de_llave], tipo_de_valor)
 */
-
 
 type Vertex struct {
 	Lat, Long float64
@@ -21,12 +20,11 @@ func main() {
 	fmt.Println(m["Bell Labs"])
 
 	m2 := map[string]Vertex{
-	"Bell Labs": {40.68433, -74.39967},
-	"Google":    {37.42202, -122.08408},
+		"Bell Labs": {40.68433, -74.39967},
+		"Google":    {37.42202, -122.08408},
 	}
 
 	fmt.Println(m2)
-
 
 	m3 := make(map[string]int)
 
@@ -41,11 +39,15 @@ func main() {
 	delete(m3, "Answer")
 	fmt.Println("The value:", m["Answer"])
 
-	// comprarbar si la llave posee sus dos valores: llva y elemento
+	// comprarbar si la llave posee sus dos valores: llave y elemento
 	v, ok := m3["Answer"]
 	fmt.Println("The value:", v, "Present?", ok)
+
+	v2, ok2 := m2["Google"]
+	fmt.Println("The value:", v2, "Present?", ok2)
+
 	/*
-		si el elemento esta en el mapa ok es true sino es false
-		si el elemento no esta en el mapa devuelve el elemento cero
+		si el elemento esta en el mapa ok es true sino es false.
+		si el elemento no esta en el mapa devuelve el elemento cero.
 	*/
 }
