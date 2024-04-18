03363a3143979477610326705b5e9bcde6100240
Aaron <aaronjimv@gmail.com>
2024-04-18 13:18:31 -0700

    agregar comentarios a fn_generica.go

--- 5_genericos/1_fn_generica/fn_generica.go
+++ 5_genericos/1_fn_generica/fn_generica.go
@@ -3,12 +3,29 @@ package main
 import "fmt"
 
 func main() {
-	PrintList("EDteam", "gophers", "Go desde cero")
-	PrintList(1, 2, 8)
+	PrintList("Hola mundo", "gophers", "Hello")
+	PrintList(1, 2, 3, 4)
 }
 
+// el `...any` le permite a la funcion recibir cualquir tipo de parametro
 func PrintList(list ...any) {
 	for _, item := range list {
 		fmt.Println(item)
 	}
 }
+
+/*
+// asi no se tiene que repetir el mismo codigo:
+
+func PrintString(list ...string) {
+	for _, item := range list {
+		fmt.Println(item)
+	}
+}
+
+func PrintInt(list ...int) {
+	for _, item := range list {
+		fmt.Println(item)
+	}
+}
+*/
