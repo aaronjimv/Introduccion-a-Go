940ab463fa57876953a554e070dbffb467c9988d
Aaron <aaronjimv@gmail.com>
2024-04-19 22:24:17 -0700

    actualizar 1_containers/

--- 7_paquetes/3_containers,sort/1_containers/containers.go
+++ 7_paquetes/3_containers,sort/1_containers/containers.go
@@ -1,21 +1,24 @@
 /*
-	Además de listas y mapas, Go tiene varias colecciones 
-	más disponibles debajo del paquete container.
+Además de listas y mapas, Go tiene varias colecciones
+más disponibles debajo del paquete container.
 
-	El paquete container/list implementa una lista 
-	doblemente vinculada.
+El paquete container/list implementa una lista
+doblemente vinculada.
 
-		nodo1 -> nodo2 -> nodo3
-			1		2		3
+	nodo1 -> nodo2 -> nodo3
+		1		2		3
 
-	Cada nodo de la lista contiene un valor (1, 2 o 3, en este caso) 
-	y un puntero al siguiente nodo. Como se trata de una lista 
-	doblemente vinculada, cada nodo también tendrá punteros al 
-	nodo anterior.
+Cada nodo de la lista contiene un valor (1, 2 o 3, en este caso)
+y un puntero al siguiente nodo. Como se trata de una lista
+doblemente vinculada, cada nodo también tendrá punteros al
+nodo anterior.
 */
 package main
 
-import ("fmt" ; "container/list")
+import (
+	"container/list"
+	"fmt"
+)
 
 func main() {
 
@@ -25,17 +28,20 @@ func main() {
 	x.PushBack(2)
 	x.PushBack(3)
 
-	for e := x.Front(); e != nil; e=e.Next() {
+	for e := x.Front(); e != nil; e = e.Next() {
 		fmt.Println(e.Value.(int))
 	}
 
 	fmt.Println(x)
 }
+
 /*
-	El valor cero para una Lista es una lista vacía 
-	(también se puede crear una * Lista usando list.New). 
+	El valor cero para una Lista es una lista vacía
+	(también se puede crear una * Lista usando list.New).
+
 	Los valores se agregan a la lista mediante PushBack.
+
 	Hacemos un bucle sobre cada elemento en
-	la lista obteniendo el primer elemento y siguiendo 
+	la lista obteniendo el primer elemento y siguiendo
 	todos los enlaces hasta llegar a cero.
 */
