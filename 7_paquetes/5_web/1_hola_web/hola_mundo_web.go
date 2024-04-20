92df675ae192f8d4f2eeef93421d54f58dddbcbe
Aaron <aaronjimv@gmail.com>
2024-04-19 22:35:22 -0700

    fix typo in hola_mundo_web.go

--- 7_paquetes/5_web/1_hola_web/hola_mundo_web.go
+++ 7_paquetes/5_web/1_hola_web/hola_mundo_web.go
@@ -1,27 +1,27 @@
-package main 
+package main
 
-import(
+import (
 	"fmt"
-	"net/http"
 	"io"
+	"net/http"
 )
 
 func main() {
-	
-	http.HandleFunc("/hola", func (w http.ResponseWriter, r *http.Request) {
-		io.WriteString(w, "holaaaaaaaa mundo web")
-	});
 
-	http.HandleFunc("/", handler) // llama a la funcion handler cuando hay /
-	http.ListenAndServe(":8000", nil) // crea un sevcidor virtual en el puerto :8000
+	http.HandleFunc("/hola", func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, "holaaaaaaaa mundo web")
+	})
+
+	http.HandleFunc("/", handler)     // llama a la funcion handler cuando hay /
+	http.ListenAndServe(":8000", nil) // crea un sevidor virtual en el puerto :8000
 }
 
-
-	/*	de tener dos parametros en ese orden
-		http.ResponseWriter es una estructura que define como se va a responder a la peticios
-	   	*http.Request es un puntero a la info de la peticion
-		w representa un script de datos de escritura
-	*/ 
+/*
+de tener dos parametros en ese orden
+  - http.ResponseWriter es una estructura que define como se va a responder a la peticion
+  - *http.Request es un puntero a la info de la peticion
+  - w representa un script de datos de escritura
+*/
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("hay una nueva peticion")
 	io.WriteString(w, "hola mundo web")
